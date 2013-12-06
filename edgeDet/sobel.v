e2ead0effd6404fd9dd6ca0d1daea6b27cf6562f
dkang9322 <wkang@mit.edu>
2013-12-05 21:41:30 -0500

    Sobel corrected with parenthesis checker and also changed dimensions of sum to have width 13

--- edgeDet/sobel.v
+++ edgeDet/sobel.v
@@ -70,17 +70,15 @@ module sobel(clock,matrix_inp,switch,edge_out);
    reg signed [13:0] abs_Gy;
 
    
-   reg [10:0] 	     sum; 
+   reg [13:0] 	     sum; 
    
    always @ (posedge clock)
      begin
 	//Gx<=((z2-z0)+((z5-z3)<<1)+(z8-z6)); //masking in x direction
-	Gx<=((z4-z0)+(z24-z20)+((z9-z5)<<2)+((z19-z15)<<2)+((z14-z10)<<2)+((z14-z10)<<1)+((z3-z1)<<1)+
-	     ((z23-z21)<<1)+((z8-z6)<<3)+((z18-z16)<<3)+((z13-z11)<<3)+((z13-z11)<<2)); //masking in x direction
+	Gx<=((z4-z0)+(z24-z20)+((z9-z5)<<2)+((z19-z15)<<2)+((z14-z10)<<2)+((z14-z10)<<1)+((z3-z1)<<1)+((z23-z21)<<1)+((z8-z6)<<3)+((z18-z16)<<3)+((z13-z11)<<3))+((z13-z11)<<2); //masking in x direction
 	
 
-	Gy<=((z0-z20)+(z4-z24)+((z1-z21)<<2)+((z3-z23)<<2)+((z2-z22)<<2)+((z2-z22)<<1)+((z5-z15)<<1))+
-	    ((z9-z19)<<1)+((z6-z16)<<3)+((z8-z18)<<3)+((z7-z17)<<3)+((z7-z17)<<2)); //masking in y direction
+	Gy<=((z0-z20)+(z4-z24)+((z1-z21)<<2)+((z3-z23)<<2)+((z2-z22)<<2)+((z2-z22)<<1)+((z5-z15)<<1))+((z9-z19)<<1)+((z6-z16)<<3)+((z8-z18)<<3)+((z7-z17)<<3)+((z7-z17)<<2); //masking in y direction
 	abs_Gx <= (Gx[13]?~Gx+1:Gx);//if negative, then invert and add  to make pos.
 	abs_Gy <= (Gy[13]?~Gy+1:Gy);//if negative, then invert and add  to make pos.
 	sum <=abs_Gx+abs_Gy;
